cc1154aa8c72d51727a63df084401e1e076fba95
sudheer1437 <sudheer1437@gmail.com>
2026-02-15 20:50:22 -0600

    Create PlaywrightFindBy annotation for element locators
    Added PlaywrightFindBy annotation with various attributes for element identification.

--- src/test/java/com/agilic/pages/PlaywrightFindBy.java
+++ src/test/java/com/agilic/pages/PlaywrightFindBy.java
@@ -1,4 +1,46 @@
 package com.agilic.pages;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD})
 public @interface PlaywrightFindBy {
+    String css() default "";
+
+    String xpath() default "";
+
+    String text() default "";
+
+    String id() default "";
+
+    String className() default "";
+
+    String attribute() default "";
+
+    String combined() default "";
+
+    String role() default "";
+
+    String name() default "";
+
+    String altText() default "";
+
+    String title() default "";
+
+    String testId() default "";
+
+    String placeholder() default "";
+
+    String byText() default "";
+
+    String shadowParentTag() default "";
+
+    String shadowElementText() default "";
+
+    String shadowByText() default "";
+
+    boolean exactMatch() default false;
 }
